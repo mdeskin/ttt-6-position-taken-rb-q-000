3744b573467d23797ed1fe78b02617df98efab2f
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-12 14:29:02 +0000

    nitrous-backup at Thu Nov 12 14:29:02 UTC 2015

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,19 +4,14 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 position = 0
 
 def position_taken?(board,position)
-
-  taken = nil
-
+    taken = nil
   if board[position] == " " || board[position] == "" || board[position] == nil
-  taken = false
-
+    taken = false
   elsif board[position] == "X" || board[position] == "O"
     taken = true
-    
+   end
+ taken
  end
-  taken
-
-end
 
 
 
